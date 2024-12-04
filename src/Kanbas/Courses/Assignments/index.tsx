514cff30fc4d234e38226574c7f86f0f7d561cee
Andy Ge <andyshge@gmail.com>
2024-12-04 15:37:22 -0500

    finshed

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -22,6 +22,7 @@ export default function Assignments() {
 
   const fetchAssignments = async () => {
     const assignments = await coursesClient.findAssignmentsForCourse(cid as string);
+    console.log(assignments)
     dispatch(setAssignments(assignments));
   };
   useEffect(() => {
